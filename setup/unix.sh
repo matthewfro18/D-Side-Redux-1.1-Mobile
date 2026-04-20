cf3671a257206f83e7ec674cc036caf3a67c4ac1
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-04-20 18:18:45 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -24,6 +24,7 @@ haxelib install hxdiscord_rpc 1.2.4
 haxelib install hxvlc 2.2.6
 haxelib install json5hx 1.0.2
 haxelib install moonchart
+haxelib git flixel-animate https://github.com/MaybeMaru/flixel-animate.git
 haxelib git extension-androidtools https://github.com/LimeExtensions/extension-androidtools.git
 haxelib git hscript-iris https://github.com/pisayesiwsi/hscript-iris dev
 haxelib git haxeui-flixel https://github.com/haxeui/haxeui-flixel 100f2c96beab619cfe72c567a058c41c71e3e998
