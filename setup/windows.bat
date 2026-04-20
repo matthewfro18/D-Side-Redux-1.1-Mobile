4b268ba4e7d76ea3507e6191d45e1e2074000cbd
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-04-20 17:59:47 -0400

    Add files via upload

---
+++ setup/windows.bat
@@ -0,0 +1,27 @@
+@echo off
+color 0a
+cd ..
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
+haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
+haxelib git lime https://github.com/kittycathy233/lime --quiet
+haxelib install openfl 9.4.2 --quiet --skip-dependencies
+haxelib install flixel 5.9.0 --quiet --skip-dependencies
+haxelib install flixel-addons 4.0.1 --quiet --skip-dependencies
+haxelib install flixel-tools 1.5.1 --quiet --skip-dependencies
+haxelib install hscript-iris 1.1.3 --quiet
+haxelib install hscript 2.7.0 --quiet
+haxelib install tjson 1.4.0 --quiet
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
+haxelib git linc_luajit https://github.com/kittycathy233/linc_luajit --quiet
+haxelib install hxdiscord_rpc --quiet --skip-dependencies
+haxelib install hxvlc 2.0.1 --quiet --skip-dependencies
+haxelib install flxgif 1.0.3 --quiet
+haxelib install flxsvg 1.1.0 --quiet
+haxelib install sl-windows-api 1.1.0 --quiet --skip-dependencies
+haxelib install hxWindowColorMode 0.2.0 --quiet --skip-dependencies
+haxelib install extension-androidtools 2.2.0 --quiet --skip-dependencies
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
+haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
+echo Finished!
+pause
