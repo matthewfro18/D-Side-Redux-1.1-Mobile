2493cd7329e8502fd7dd848f0bd4feeb770bf742
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-04-21 06:23:32 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -29,6 +29,7 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib git lime https://github.com/matthewfro18/lime
+haxelib git flixel https://github.com/HaxeFlixel/flixel
 haxelib install format
 haxelib install hxp
 echo Finished!
